7f6453a2dc034ab1ea726b29efd7b6148913bd2e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-05 01:38:15 -0500

    Workaround for kernel modules

--- armv6l-linux.nix
+++ armv6l-linux.nix
@@ -6,4 +6,11 @@
   nixpkgs.crossSystem = {
     system = "armv6l-linux";
   };
+
+  nixpkgs.overlays = [
+    (final: super: {
+      # modprobe: FATAL: Module ahci not found in directory /nix/store/scal01jizlpzvpshhda0gpv3a0vwv7kx-linux-armv6l-unknown-linux-gnueabihf-5.10.52-1.20210805-modules/lib/modules/5.10.52
+      makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
 }
