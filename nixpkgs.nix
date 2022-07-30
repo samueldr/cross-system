9fc60f68583846d0737ddab4dd9a8a422d51762c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-07-30 14:03:35 -0400

    XXX 22.05

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,7 +1,7 @@
 let
-  revision = "d5f237872975e6fb6f76eef1368b5634ffcd266f";
+  revision = "9370544d849be8a07193e7611d02e6f6f1b10768";
 in
 import (builtins.fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/${revision}.tar.gz";
-  sha256 = "0fsjwhqgxyd2v86glr2560gr3zx9mb6fhllydmrxi5i04c549vsr";
+  sha256 = "0g61bfs8q5y3z73ms2gf8hlaxifr77rmqbfi0xbbp9lv61pz0ijd";
 })
