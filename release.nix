ffc2d3327615ca3a4cb56a16a6bec425699464a9
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-01-21 14:21:42 -0500

    release.nix: Add

---
+++ release.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import (import ./npins).nixpkgs {} }:
+let
+  default = import ./default.nix { inherit pkgs; };
+
+  renamed =
+    builtins.listToAttrs (
+      builtins.concatLists (
+        builtins.attrValues (
+          builtins.mapAttrs (
+            system: images:
+            builtins.map (
+              name:
+              {
+                name = "${system}/${name}";
+                value = images.${name};
+              }
+            ) (builtins.attrNames images)
+          ) default
+        )
+      )
+    )
+  ;
+in
+renamed
