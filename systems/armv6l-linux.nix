fad828980901d8488d910334ac4e9661f70d2aff
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-01-25 16:59:26 -0500

    armv6l-linux: Use mainline
    Might or might not work on a raspberry pi, but the raspberry pi kernel
    does not build at this time.
    
    Anyways, this is more about tracking cross-compilation issues than
    making directly useful images.

--- systems/armv6l-linux.nix
+++ systems/armv6l-linux.nix
@@ -1,4 +1,14 @@
+{ lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    mkForce
+  ;
+in
 {
+  # "raspberrypi" images default to: `pkgs.linuxKernel.packages.linux_rpi1`
+  boot.kernelPackages = mkForce pkgs.linuxPackages_latest;
+
   nixpkgs.overlays = [
     (final: super: {
       # modprobe: FATAL: Module ahci not found in directory /nix/store/scal01jizlpzvpshhda0gpv3a0vwv7kx-linux-armv6l-unknown-linux-gnueabihf-5.10.52-1.20210805-modules/lib/modules/5.10.52
