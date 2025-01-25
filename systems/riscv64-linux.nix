84a9f83f1330f737e7b1cfce5024ede22a154910
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-01-25 17:21:18 -0500

    riscv64-linux: Fix grub build for EFI iso

--- systems/riscv64-linux.nix
+++ systems/riscv64-linux.nix
@@ -1,2 +1,16 @@
 {
+  nixpkgs.overlays = [(final: super: {
+    grub2 = super.grub2.overrideAttrs({ patches ? [], ... }: {
+      patches = patches ++ [
+        # grub-mkimage: error: relocation 0x2b is not implemented yet.
+        # https://savannah.gnu.org/bugs/?65909
+        # https://lists.gnu.org/archive/html/bug-grub/2024-06/msg00012.html
+        # (Alternatively, gcc13Stdenv could be used.)
+        (super.fetchpatch {
+          url = "https://file.savannah.gnu.org/file/0263-Use-medany-instead-of-large-model-for-RISCV.patch?file_id=56184";
+          hash = "sha256-s6uPdM6vug+7lZ1FPhup+P4uFRO02+YtbVxDUDn1WLc=";
+        })
+      ];
+    });
+  })];
 }
