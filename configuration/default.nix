dcdb77a891159486070b6028d199aab642efedd6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-05-27 23:21:18 -0400

    configuration: Drop now unneeded screen workaround

--- configuration/default.nix
+++ configuration/default.nix
@@ -7,18 +7,6 @@
   ];
 
   nixpkgs.overlays = [(final: super: {
-    screen = super.screen.overrideAttrs({ patches ? [], ... }: {
-      patches =
-        patches ++ [
-          # Cross-compilation workaround post GCC 14
-          (super.fetchpatch {
-            url = "https://raw.githubusercontent.com/openwrt/packages/59db1470314a4a3f5c78ad805fa8381de0231936/utils/screen/patches/010-ptyh.patch";
-            hash = "sha256-pDlSGKQYGz0lSXzgqFmGLxFf6XnShzI2Vg0VLMy354M=";
-          })
-        ]
-      ;
-    });
-
     #
     # armv7l and armv6l:
     #
