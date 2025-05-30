f0ba0d5405aea9bcaca354f0d527ddb9784a3d76
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-05-30 14:12:23 -0400

    armv6l-linux: Work around U-Boot regression

--- systems/armv6l-linux.nix
+++ systems/armv6l-linux.nix
@@ -13,6 +13,18 @@ in
     (final: super: {
       # modprobe: FATAL: Module ahci not found in directory /nix/store/scal01jizlpzvpshhda0gpv3a0vwv7kx-linux-armv6l-unknown-linux-gnueabihf-5.10.52-1.20210805-modules/lib/modules/5.10.52
       makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+
+      # Mainline U-Boot may have broken build for armv6l U-Boot
+      ubootRaspberryPi = super.ubootRaspberryPi.override {
+        extraConfig = ''
+           # CONFIG_EFI_LOADER is not set
+        '';
+      };
+      ubootRaspberryPiZero = super.ubootRaspberryPiZero.override {
+        extraConfig = ''
+           # CONFIG_EFI_LOADER is not set
+        '';
+      };
     })
   ];
 }
