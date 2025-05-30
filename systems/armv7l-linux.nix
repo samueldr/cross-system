90e8fea1131a010466375d21854c27b2e631fa81
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-05-30 00:59:09 -0400

    armv7l-linux: Also work around missing kernel modules

--- systems/armv7l-linux.nix
+++ systems/armv7l-linux.nix
@@ -1,4 +1,6 @@
 {
   nixpkgs.overlays = [(self: super: {
+    # modprobe: FATAL: Module hid_lenovo not found in directory ...
+    makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
   })];
 }
