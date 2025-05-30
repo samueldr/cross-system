5c6da31a385fdf703943b7a7389f38416db72cb4
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-05-30 00:58:58 -0400

    armv7l-linux: Drop now unneeded workaround

--- systems/armv7l-linux.nix
+++ systems/armv7l-linux.nix
@@ -1,14 +1,4 @@
 {
   nixpkgs.overlays = [(self: super: {
-    # Works around libselinux failure with python on armv7l.
-    # LONG_BIT definition appears wrong for platform
-    libselinux = (super.libselinux
-      .override({
-        enablePython = false;
-      }))
-      .overrideAttrs (_: {
-        preInstall = ":";
-      })
-    ;
   })];
 }
